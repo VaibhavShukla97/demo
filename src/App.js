29c30fbaa1b26c5122705296fba54f829e713134
Vaibhav Shukla <vaibhavshukla789@gmail.com>
2022-03-27 23:12:03 +0530

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="container my-5">
       <div className="card text-center my-5">
         <div className="card-body">
-          <h1>  Counter Pre-Post App    </h1>
+          <h1>  Counter Pre-Post Application   </h1>
           <div className="my-5">
             <h2 className="my-5" >  {count} </h2>
             <button className="btn btn-success mx-3" onClick = {()  => setCount(count + 1) } disabled= {count === 10}>+</button>
